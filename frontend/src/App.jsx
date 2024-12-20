5bcc0fb607e951f4df4fdb6ff7de035721142d3b
Esan Tolulope <99842417+BigZaddy0@users.noreply.github.com>
2024-12-20 15:52:27 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -51,9 +51,9 @@ function App() {
  
   return (
     <div className="min-h-screen min-w-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center reletive overflow-hidden">
-      <FloatingShape color='bg-green-500' size='w-64 h-64' top='-5%' left='10%' delay={0} />
-      <FloatingShape color='bg-emerald-500' size='w-48 h-48' top='70%' left='80%' delay={5} />
-      <FloatingShape color='bg-lime-500' size='w-32 h-32' top='40%' left='-10%' delay={2} />
+      <FloatingShape color='bg-green-500' size='w-30 h-30' top='-5%' left='0%' delay={0} />
+      <FloatingShape color='bg-emerald-500' size='w-24 h-24' top='70%' left='0%' delay={5} />
+      <FloatingShape color='bg-lime-500' size='w-32 h-32' top='40%' left='0%' delay={2} />
       
       <Routes>
         <Route path='/' element={<ProtectedRoute>
